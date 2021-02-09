3fdeac5fc35fb76b1bbfcdedf5b0aaccda4f6ff4
Samarth Shah <samarthcodes3.14@gmail.com>
2021-02-09 09:27:28 -0800

    initial commit

---
+++ Lab1/BSTreeRunner.java
@@ -0,0 +1,37 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import static java.lang.System.*;
+import java.util.*;
+
+public class BSTreeRunner
+{
+   public static void main( String args[] )
+   {
+ 		//add test cases here
+ 		
+ 		
+		BinarySearchTree bst = new BinarySearchTree();
+ 		bst.add(90);
+ 		bst.add(80);
+ 		bst.add(70);
+ 		bst.add(100);
+ 		bst.add(98);
+ 		bst.add(120);
+ 		bst.add(85);
+ 		// System.out.println("In Order: "); 
+ 		// bst.inOrder();
+ 		// System.out.println("Pre Order: "); 
+ 		// bst.preOrder();
+ 		// System.out.println("Post Order: "); 
+ 		// bst.postOrder();
+ 		// System.out.println("Reverse Order: "); 
+ 		// bst.revOrder();
+ 		
+ 		
+ 		
+ 
+   }
+}
---
+++ Lab1/speedTest.py
@@ -0,0 +1,2 @@
+for i in range(0, 90000000):
+	print(i)
